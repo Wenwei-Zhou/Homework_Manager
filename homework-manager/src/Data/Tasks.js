767552d37728ee90e8bc66419acb28607f607820
Wenwei-Zhou <wenweizhou0@gmail.com>
2025-10-11 01:38:47 +1100

    updated course
    updated course, created Tasks.js file and modified the tasks list from Course.jsx file to Tasks.js file.

---
+++ homework-manager/src/Data/Tasks.js
@@ -0,0 +1,27 @@
+const taskList = [
+  {
+    id: 1,
+    title: "Complete calculation exercises",
+    course: "maths",
+    dueDate: "2025-05-12",
+    completed: false,
+    priority: "high",
+  },
+  {
+    id: 2,
+    title: "Read article and write essay",
+    course: "English",
+    dueDate: "2025-05-15",
+    completed: false,
+    priority: "medium",
+  },
+  {
+    id: 3,
+    title: "Force and motion problem set",
+    course: "Physics",
+    dueDate: "2025-05-10",
+    completed: true,
+    priority: "high",
+  },
+];
+export default taskList;
